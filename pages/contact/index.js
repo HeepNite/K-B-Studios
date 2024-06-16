9e7a69a160978a04ae1ad5c0d95b8629a073f18e
Mariano Barrionuevo <mmbalarcon@gmail.com>
2024-06-16 13:15:08 -0400

    Updated

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -12,7 +12,7 @@ const Contact = () => {
       <div className="container mx-auto py-32 text-center xl:text-left flex justify-center items-center">
         <div className=" flex flex-col justify-center w-full max-w-[700px]">
           <motion.h2 className="h2 text-center mb-12" variants={fadeIn('right', 0.4)} initial='hidden' animate='show' exit='hidden'>
-            let's <span className="text-accent"> Connect </span>
+            let&apos;s <span className="text-accent"> Connect </span>
           </motion.h2>
 
           <motion.form className="flex-1 flex items-center flex-col gap-6 w-full mx-auto" variants={fadeIn('left', 0.4)} initial='hidden' animate='show' exit='hidden'>
@@ -24,7 +24,7 @@ const Contact = () => {
             <textarea className="textarea" placeholder="Message" name="" id="" cols="30" rows="10"></textarea>
 
             <button className="btn rounded-lg border border-white/50 max-w-[14rem] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
-              <span className=" group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500"> I'm waiting for you!</span>
+              <span className=" group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500"> I&apos;m waiting for you!</span>
               <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
             </button>
           </motion.form>
